bb52ec161c551021c8b3cd71ef06abd00697e89d
ArthurBR20 <31294294+ArthurBR20@users.noreply.github.com>
2019-10-18 19:27:40 -0300

    Add files via upload

---
+++ cliente-api/src/main/java/br/com/unipac/cliente/api/clienteapi/resources/ClienteDAO.java
@@ -0,0 +1,11 @@
+package br.com.unipac.cliente.api.clienteapi.resources;
+
+public interface ClienteDAO {
+
+	boolean salvar(Cliente cliente);
+	boolean alterar(Long id, Cliente cliente);
+	
+	List<Cliente> listarTodos();
+	boolean buscarPorId(Cliente cliente);
+	boolean removerPorId(Cliente cliente);
+	}
